fafd60b97f325dcd1c971f547343e49f9ce19ee7
Alexey Zyukov <aalex64@ukr.net>
2021-11-29 19:02:09 +0200

    TransactionHistory update id

--- src/components/4_TransactionHistory/TransactionHistoy.js
+++ src/components/4_TransactionHistory/TransactionHistoy.js
@@ -1,18 +1,23 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import TableHead from './TableHead';
 import TableBody from './TableBody';
 
 export default function TransactionHistory({ props }) {
   return (
-    <>
+    <table class="transaction-history">
       <TableHead />
       {props.map(prop => (
         <TableBody
+          key={prop.id}
           type={prop.type}
           amount={prop.amount}
           currency={prop.currency}
         />
       ))}
-    </>
+    </table>
   );
 }
+
+TransactionHistory.propTypes = {
+  key: PropTypes.number.isRequired,
+};
