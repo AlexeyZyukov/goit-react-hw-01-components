94b7de23918e07571dd143696644617305784e58
Alexey Zyukov <aalex64@ukr.net>
2021-12-01 16:21:18 +0200

    u[date transaction styles

--- src/components/4_TransactionHistory/TableBody.js
+++ src/components/4_TransactionHistory/TableBody.js
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
-// import styles from './transaction.module.css';
+import styles from './transaction.module.css';
 
 export default function TableBody({ type, amount, currency }) {
   return (
     <tbody>
-      <tr>
+      <tr className={styles.row}>
         <td>{type}</td>
         <td>{amount}</td>
         <td>{currency}</td>
